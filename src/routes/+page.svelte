589772a3ef683b7720a03b0505d1a2403a168903
kolown2007 <kolown@gmail.com>
2025-12-04 22:12:36 +0800

    added dropcall icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,59 +2,62 @@
 	const src = `/lbd.svg`;
 	const cyber_purok = `/cyber_purok.png`;
 	const powerplant = `/powerplant.png`;
+	const dropcall = `/dropcall.png`;
+
+
 </script>
 
-<main class="min-h-screen w-full flex flex-col overflow-x-hidden">
-	
+<main class="flex min-h-screen w-full flex-col overflow-x-hidden">
+	<section
+		id="about"
+		class="flex w-full flex-col items-center justify-center bg-green-700 py-5 text-amber-50"
+	>
+		<div class="mx-auto max-w-6xl space-y-9 px-4">
+			<a href="https://cyberpurok.lowbandwidthdreams.com/">
+				<img
+					src={cyber_purok}
+					alt="lbd"
+					loading="lazy"
+					class="mx-auto block w-full max-w-xs object-contain md:max-w-sm lg:max-w-md"
+				/>
+			</a>
 
-<section id="about" class="flex flex-col items-center justify-center bg-green-700 text-amber-50 py-5 w-full">
-
-	<div class="max-w-6xl mx-auto px-4 space-y-9">
-
-
-	
-
-	
-
-	<a href="https://cyberpurok.lowbandwidthdreams.com/">
-		<img
-			src={cyber_purok}
-			alt="lbd"
-			loading="lazy"
-			class="w-full max-w-sm mx-auto md:max-w-md lg:max-w-xl block object-contain"
-		/>
-	</a>
-
-	
-
-			<a href="/panawagan_banner" class="text-center text-amber-50 text-3xl md:text-4xl lg:text-5xl block mt-4">
-				<p class ="text-6xl">Error 405: </p>
+			<a
+				href="/panawagan_banner"
+				class="mt-4 block text-center text-3xl text-amber-50 md:text-4xl lg:text-5xl"
+			>
+				<p class="text-6xl">Error 405:</p>
 				<p class="text-3xl">Panawagan Cannot Be Fulfilled</p>
 			</a>
 			<a href="/power_plant">
-		<img
-			src={powerplant}
-			alt="lbd"
-			loading="lazy"
-			class="w-full max-w-sm mx-auto md:max-w-md lg:max-w-xl block object-contain"
-		/>
-	</a>
+				<img
+					src={powerplant}
+					alt="lbd"
+					loading="lazy"
+					class="mx-auto block w-full max-w-xs object-contain md:max-w-sm lg:max-w-md"
+				/>
+			</a>
 
-			
-			<a href="https://yellowbasket.lowbandwidthdreams.com/" class="text-center text-amber-50 text-3xl md:text-4xl lg:text-5xl block mt-4">
+			<a
+				href="https://yellowbasket.lowbandwidthdreams.com/"
+				class="mt-4 block text-center text-3xl text-amber-50 md:text-4xl lg:text-5xl"
+			>
 				Yellow Basket
 			</a>
 
-			<a href="https://kolown.net/dropcall" class="text-center text-amber-50 text-3xl md:text-4xl lg:text-5xl block mt-4">
-				Drop_Call
+	
+					<a href="https://kolown.net/H6tR9yWvB3q29kwnfnp">
+				<img
+					src={dropcall}
+					alt="lbd"
+					loading="lazy"
+					class="mx-auto block w-full max-w-xs object-contain md:max-w-sm lg:max-w-md"
+				/>
 			</a>
 
-			<a href="/" class="text-center text-amber-50 text-3xl md:text-4xl lg:text-5xl block mt-4">
+			<a href="/" class="mt-4 block text-center text-3xl text-amber-50 md:text-4xl lg:text-5xl">
 				Aninipot
 			</a>
-
-	</div>
-
-</section>
-
+		</div>
+	</section>
 </main>
